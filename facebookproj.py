a8f83403e0c8bff52a388ec56f954658f6dc2d16
eviatar21-meet <52971705+eviatar21-meet@users.noreply.github.com>
2019-11-20 20:04:47 +0200

    Add files via upload

--- facebookproj.py
+++ facebookproj.py
@@ -1,10 +1,16 @@
+
+posts=[]
+users=[]
+comments=[]
+
+
 class User(object):
 	def __init__ (self,name,email,password):
 		self.name=name
 		self.email=email
 		self.password=password
 		self.friends_list=[]
-		self.posts = []
+		users.append(self)
 
 	def add_friend (self, email):
 		self.friends_list.append(email)
@@ -14,22 +20,54 @@ class User(object):
 		self.friends_list.remove(email)
 		print (self.name + " has removed " + email + " as a friend.")
 
-	def post (self, text):
-		self.posts.append(text)
-		print (self.name + " has posted: " + text )
+	def add_post (self,p_text,date):
+		post1=Post(p_text,date,self)
+		posts.append(post1)
+		print (self.name + " has posted: " + p_text  + " on the date " + date)
 
 	def get_userInfo (self):
-		print("Name: ["+self.name+"]"+"Email: ["+self.email+"]"+"Password: ["+self.password+"]"+"Friends: ["+str(self.friends_list)+']'+'Posts: ['+str(self.posts)+']')
+		my_posts=[]
+		for x in posts:
+			if self == x.author:
+				my_posts.append(x)
+		print("Name: ["+self.name+"]"+"Email: ["+self.email+"]"+"Password: ["+self.password+"]")
+		print("Friends: "+str(self.friends_list)+'Posts: '+str(my_posts))
 
 class Post(object):
-	def __init__ (self)
+	def __init__ (self,p_text,date,author):		
+		self.author=author
+		self.text=p_text
+		self.date=date
 
+	def create_comment(self,c_text,date,author):
+		comment1=Comment(c_text,date,self)
+		comments.append(comment1)
+		print("has posted" + self.c_text)
 
+	def remove_comment(self):
+		self.comments.remove(c_text)
+
+	"""
+	def post_date(self):
+		print("the post was posted on "+ self.date)
+
+	def edit_comment(self,new_c_text):
+		self.c_text=new_c_text
+		print("the comment has been changed:")
+	def __repr__(self):
+		return self.text
+		"""
+
+class Comment(Post):
+	def __init__(self):
+		Post.__init__(self)
+		self.c_text=c_text
+		
 
 user1 = User("jesus","jesus@meet.mit.edu", "imalive")
 user2 = User("moses","moses@meet.mit.edu", "imdead")
 user2.add_friend("jesus@meet.mit.edu")
-user1.post("I am the way and the truth and the life. No one comes to the Father except through me. John 14:6 NIV")
-user1.get_userInfo()
+user1.add_post("whats up?","10.5.0")
+user1.get_userInfo()	
 user2.get_userInfo()
 user2.remove_friend("jesus@meet.mit.edu")
