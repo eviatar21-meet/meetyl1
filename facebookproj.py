be83266127ddeb12829df5c61f77c1355e453c5e
eviatar21-meet <52971705+eviatar21-meet@users.noreply.github.com>
2019-12-01 22:11:14 +0200

    Add files via upload

--- facebookproj.py
+++ facebookproj.py
@@ -62,25 +62,67 @@ class Comment(Post):
 		self.text=text
 		
 
-user1 = User("jesus","jesus@meet.mit.edu", "imalive")
-user2 = User("moses","moses@meet.mit.edu", "imdead")
+user1 = User("matt","matt@meet.mit.edu", "1234")
+user2 = User("dave","dave@meet.mit.edu", "9876")
+user0 = ""
 
-iemail=input("what is your email?")
-ipass = input("what is your password?")
-for x in users:
-	if iemail == x.email and ipass == x.password:
-		print(x.name + "you are logged in!")
+
+start_menu=""
+while start_menu != "1" and start_menu != "2":
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	start_menu = input("LOG IN - PRESS 1 \n SIGN UP - PRESS 2 \n ||")
+if start_menu == "1":
+	iemail=input("what is your email?")
+	ipass = input("what is your password?")
+	for x in users:
+		if iemail == x.email and ipass == x.password:
+			print("you are logged in as " + x.name)
+			user0 = x
+			break
+		else:
+			print("you are not a registerd user!")
+			quit()
+elif start_menu == "2":
+	aname = input("enter your name: \n ||")
+	aemail = input("enter your email: \n ||")
+	apass = input("create a password: \n ||")
+	user0 = User(aname,aemail,apass)
+	print("you are logged in as " + aname)
+
+
+activity_menu=""
+while 1==1:
+	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	activity_menu = input("POST - PRESS 1 \n COMMENT - PRESS 2 \n ADD FRIEND - PRESS 3 \n REMOVE FRIEND - PRESS 4 \n SURF - PRESS 5 \n QUIT - PRESS 6 \n||")
+	if activity_menu == "1":
+		title=input("enter title: ")
+		date=input("enter date: ")
+		text=input("enter text: ")
+		user0.add_post(text,date,0,title)
+	elif activity_menu == "2":
+		title=input("enter title: ")
+		date=input("enter date: ")
+		text=input("enter text: ")
+		user0.create_comment(text,date,0,title)
+	elif activity_menu == "3":
+		friend=input("enter friend's email: ")
+		user0.add_friend(friend)
+	elif activity_menu == "4":
+		friend=input("enter friend's email: ")
+		user0.remove_friend(friend)
+	elif activity_menu == "5":
+		break
+	elif activity_menu == "6":
+		print("good bye!")
 	else:
-		quit()
+		print("you entered an invalid input")
 
 
 
-
-
-user2.add_friend("jesus@meet.mit.edu")
-user1.add_post("whats up?","10.5.0",2,"thepost")
-user1.get_userInfo()	
-user2.get_userInfo()
-user2.remove_friend("jesus@meet.mit.edu")
+user2.add_friend("matt@meet.mit.edu")
+user1.add_post("whats up?","10.5.2019",2,"thepost")
+#user1.get_userInfo()	
+#user2.get_userInfo()
+user2.remove_friend("matt@meet.mit.edu")
 
 user1.create_comment("lejfmdirkejs","5.2.2002",64,"thepost")
